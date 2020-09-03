147c82e50687351671708434b555b6fa55f2c745
Alex Manoel Ferreira Silva <alex@legytma.com.br>
2020-09-03 11:22:54 -0300

    refactor: update project example
    Update project example.
    
    Signed-off-by: Alex Manoel Ferreira Silva <alex@legytma.com.br>

---
+++ example/android/app/src/main/kotlin/Legytma/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package Legytma.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/android/app/src/main/kotlin/br/com/legytma/schemawidget/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.legytma.schemawidget.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
